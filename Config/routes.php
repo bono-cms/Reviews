ffafa1f1077a70fd9309e906a251c9dbabc4cc69
David Yang <daworld.ny@gmail.com>
2016-05-07 22:20:10 +0500

    Replaced static admin segment with its placeholder

--- Config/routes.php
+++ Config/routes.php
@@ -14,42 +14,42 @@ return array(
         'controller' => 'Reviews@indexAction'
     ),
     
-    '/admin/module/reviews/config' => array(
+    '/%s/module/reviews/config' => array(
         'controller' => 'Admin:Config@indexAction'
     ),
     
-    '/admin/module/reviews/config.ajax' => array(
+    '/%s/module/reviews/config.ajax' => array(
         'controller' => 'Admin:Config@saveAction',
         'disallow' => array('guest')
     ),
     
-    '/admin/module/reviews' => array(
+    '/%s/module/reviews' => array(
         'controller' => 'Admin:Review@gridAction'
     ),
     
-    '/admin/module/reviews/tweak' => array(
+    '/%s/module/reviews/tweak' => array(
         'controller' => 'Admin:Review@tweakAction',
         'disallow' => array('guest')
     ),
     
-    '/admin/module/reviews/page/(:var)' => array(
+    '/%s/module/reviews/page/(:var)' => array(
         'controller' => 'Admin:Review@gridAction'
     ),
     
-    '/admin/module/reviews/add' => array(
+    '/%s/module/reviews/add' => array(
         'controller' => 'Admin:Review@addAction'
     ),
     
-    '/admin/module/reviews/edit/(:var)' => array(
+    '/%s/module/reviews/edit/(:var)' => array(
         'controller' => 'Admin:Review@editAction'
     ),
     
-    '/admin/module/reviews/save' => array(
+    '/%s/module/reviews/save' => array(
         'controller' => 'Admin:Review@saveAction',
         'disallow' => array('guest')
     ),
     
-    '/admin/module/reviews/delete/(:var)' => array(
+    '/%s/module/reviews/delete/(:var)' => array(
         'controller' => 'Admin:Review@deleteAction',
         'disallow' => array('guest')
     )
