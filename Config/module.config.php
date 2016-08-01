e0ce5bf6e0b436339232787f6cdef125e288aa83
David Yang <daworld.ny@gmail.com>
2016-08-01 22:29:14 +0500

    Removed extras from module configuration file

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,11 +1,9 @@
 <?php
 
 return array(
-    'module' => array(
-        'name' => 'Reviews',
-        'route' => 'Reviews:Admin:Review@gridAction',
-        'icon' => 'fa fa-comments-o fa-5x',
-        'order' => 1,
-        'description' => 'Reviews module allows you to make a guest book on your site'
-    )
+ 'name' => 'Reviews',
+ 'route' => 'Reviews:Admin:Review@gridAction',
+ 'icon' => 'fa fa-comments-o fa-5x',
+ 'order' => 1,
+ 'description' => 'Reviews module allows you to make a guest book on your site'
 );
