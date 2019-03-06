9be6881e5828429a99de2683162ce974ef5a04d6
dave-yang <daworld.ny@gmail.com>
2019-03-06 23:08:36 +0500

    Update module.config.php

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,9 +1,28 @@
 <?php
 
+/**
+ * Module configuration container
+ */
+
 return array(
- 'name' => 'Reviews',
- 'caption' => 'Reviews',
- 'route' => 'Reviews:Admin:Review@gridAction',
- 'order' => 1,
- 'description' => 'Reviews module allows you to make a guest book on your site'
+    'name' => 'Reviews',
+    'description' => 'Reviews module allows you to make a guest book on your site',
+    'menu' => array(
+        'name' => 'Reviews',
+        'icon' => 'fas fa-frown-open',
+        'items' => array(
+            array(
+                'route' => 'Reviews:Admin:Review@gridAction',
+                'name' => 'View all reviews'
+            ),
+            array(
+                'route' => 'Reviews:Admin:Review@addAction',
+                'name' => 'Add new review'
+            ),
+            array(
+                'route' => 'Reviews:Admin:Config@indexAction',
+                'name' => 'Configuration'
+            )
+        )
+    )
 );
